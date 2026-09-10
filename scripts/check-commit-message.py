2c42488402ea76581490f095b4b57c8a6fa24d64
fwojciechoo <filipwojciechowski2806@gmail.com>
2026-09-10 14:28:20 +0200

    test: hook test

--- scripts/check-commit-message.py
+++ scripts/check-commit-message.py
@@ -1,1 +1,13 @@
-print("just checking id it starts")
+import re
+import sys
+
+pattern = r"^(feat|fix|test|docs|refactor|chore|ci)(\([a-z0-9_-]+\))?!?: .+"
+
+message = open(sys.argv[1], encoding="utf_8").read().splitlines()[0]
+
+if re.match(pattern, message):
+    print("Valid conventional commit message")
+    sys.exit(0)
+else:
+    print("Invalid conventional commit message")
+    sys.exit(1)
